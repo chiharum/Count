b7a02b7fd50cae7a4883c85618b6815d745ee4fe
Chiharu Miyoshi <chiharu-miyosi@chiharu-miyoshi.local>
2015-04-03 17:11:15 +0900

    少し変えた

--- app/src/main/java/com/example/chiharu_miyosi/count/MainActivity.java
+++ app/src/main/java/com/example/chiharu_miyosi/count/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    // 変えてみます
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
